e7db5e26d127aa7b2633620744a7371db7a86bc3
James Abad <rjames1295@gmail.com>
2020-05-03 02:56:07 +0300

    Updates

---
+++ precache-manifest.c2c6cd715a76827b04561bfa9d0ddef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d92b07cc4c1a160939faf6291dbbdd9",
+    "url": "/csv-dnd/index.html"
+  },
+  {
+    "revision": "0d51bbd8c18567c3caff",
+    "url": "/csv-dnd/static/js/2.065b6796.chunk.js"
+  },
+  {
+    "revision": "260254c2b55f2376c2a0340f77dec2ca",
+    "url": "/csv-dnd/static/js/2.065b6796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f88fe5c6ce48802e55",
+    "url": "/csv-dnd/static/js/main.48f29b0b.chunk.js"
+  },
+  {
+    "revision": "42f1f429aa5180006076",
+    "url": "/csv-dnd/static/js/runtime-main.1a2a5664.js"
+  }
+]);
